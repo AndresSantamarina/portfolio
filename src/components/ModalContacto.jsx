d16858fe14a993865835f0c3b4f8d2433bec5cd3
Andres Santamarina <andresesantamarina@gmail.com>
2024-03-02 20:36:25 -0300

    agregue la logica para poder enviar un mail con sus validaciones necesarias

--- src/components/ModalContacto.jsx
+++ src/components/ModalContacto.jsx
@@ -3,6 +3,8 @@ import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
 
 function ModalContacto() {
   const [show, setShow] = useState(false);
@@ -13,10 +15,17 @@ function ModalContacto() {
   const template = import.meta.env.VITE_TEMPLATE_KEY;
   const publicKey = import.meta.env.VITE_PUBLIC_KEY;
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+
   const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const sendEmail = () => {
+
     emailjs
       .sendForm(servicio, template, form.current, {
         publicKey: publicKey,
@@ -25,9 +34,21 @@ function ModalContacto() {
         (result) => {
           console.log(result.text);
           console.log("SUCCESS!");
+          Swal.fire({
+            title: "Mensaje enviado!",
+            text: `El email fue enviado correctamente`,
+            icon: "success",
+          });
+          handleClose();
+          reset();
         },
         (error) => {
           console.log("FAILED...", error.text);
+          Swal.fire({
+            title: "Ocurrió un error",
+            text: `El email no pudo ser enviado, intente esta operación en unos minutos.`,
+            icon: "error",
+          });
         }
       );
   };
@@ -46,7 +67,7 @@ function ModalContacto() {
           <Modal.Title className="text-teal-600">Contactame!</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form ref={form} onSubmit={sendEmail}>
+          <Form ref={form} onSubmit={handleSubmit(sendEmail)}>
             <Form.Group className="mb-3" controlId="nombreInput">
               <Form.Label>Ingresa tu nombre</Form.Label>
               <Form.Control
@@ -54,7 +75,21 @@ function ModalContacto() {
                 name="user_name"
                 placeholder="Tu nombre"
                 autoFocus
+                {...register("user_name", {
+                  required: "El nombre es obligatorio",
+                  minLength: {
+                    value: 2,
+                    message: "El nombre debe tener como mínimo 2 caracteres",
+                  },
+                  maxLength: {
+                    value: 40,
+                    message: "El nombre debe tener como máximo 40 caracteres",
+                  },
+                })}
               />
+              <Form.Text className="text-danger">
+                {errors.user_name?.message}
+              </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="mailInput">
               <Form.Label>Ingresa tu mail</Form.Label>
@@ -63,17 +98,45 @@ function ModalContacto() {
                 name="user_email"
                 placeholder="nombre@gmail.com"
                 autoFocus
+                {...register("user_email", {
+                  required: "El mail es obligatorio",
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: "Debe ingresar una dirección de correo válida",
+                  },
+                })}
               />
+              <Form.Text className="text-danger">
+                {errors.user_email?.message}
+              </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="mensajeInput">
               <Form.Label>Ingresá tu mensaje</Form.Label>
-              <Form.Control as="textarea" rows={3} name="message" />
+              <Form.Control
+                as="textarea"
+                rows={3}
+                name="message"
+                {...register("message", {
+                  required: "El mensaje es obligatorio",
+                  minLength: {
+                    value: 5,
+                    message: "El mensaje debe tener como mínimo 5 caracteres",
+                  },
+                  maxLength: {
+                    value: 500,
+                    message: "El mensaje debe tener como máximo 500 caracteres",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.message?.message}
+              </Form.Text>
             </Form.Group>
             <button
               className="bg-teal-500 hover:bg-teal-600 px-4 mx-2 py-2 rounded-md md:mx-5 text-white"
-              onClick={handleClose}
               type="submit"
               value="Send"
+              onClick={handleSubmit(sendEmail)}
             >
               Enviar
             </button>
